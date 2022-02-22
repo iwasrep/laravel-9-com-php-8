bf9617ad81b90b5424e223ebe2eda2dd70ee1938
root <root@PCIWAS.localdomain>
2022-02-22 18:24:28 -0300

    Atualizando home e rotas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ env('APP_NAME') }}</title>
+        <meta name="description" content="{{ env('APP_DESCRIPTION') }}">
+        <meta name="author" content="{{ env('APP_DEV') }}">
+        <meta name="robots" content="noindex, nofollow">
+
+        <!-- Icons -->
+        <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+        <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function(){ return 'Login'; })->name('login');
+Route::get('/register', function(){ return 'Register'; })->name('register');
+Route::get('/reset', function(){ return 'Reset Password'; })->name('reset');
